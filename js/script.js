6e382fa564f1f3bd6bdc0920954e01e3a885c039
Rodrigo <rodrigovinicius.bsb@gmail.com>
2022-12-20 16:01:12 -0300

    clear fetch

--- js/script.js
+++ js/script.js
@@ -6,6 +6,7 @@ let menuBtn = document.querySelector('.menu-icon span');
 
 let titleMain = document.querySelector('.titleMain');
 
+let all = document.getElementById('getAllDigimon');
 let training = document.getElementById('getTrainingDigimon');
 let rookie = document.getElementById('getRookieDigimon');
 let champion = document.getElementById('getChampionDigimon');
@@ -36,11 +37,6 @@ searchBtn.onclick = () => {
 };
 
 async function getAllDigimons() {
-  displayNoneDigimon();
-  navBarAnchors.forEach((element) => element.classList.remove('active'));
-
-  document.getElementById('getAllDigimon').classList.remove('hide');
-
   titleMain.innerText = 'Lista com todos os Digimons';
 
   let url = 'https://digimon-api.vercel.app/api/digimon';
@@ -48,146 +44,135 @@ async function getAllDigimons() {
   let data = await response.json();
 
   localStorage.setItem('digimon', JSON.stringify(data));
-
-  data.map((digimon) => {
-    //card element
-    let divCard = document.createElement('div');
-    divCard.className = 'container';
-    document.getElementById('getAllDigimon').appendChild(divCard);
-
-    //div image
-    let divImg = document.createElement('div');
-    let img = document.createElement('img');
-    divImg.classList.add('image');
-    img.setAttribute('src', `${digimon.img}`);
-    divImg.appendChild(img);
-    divCard.appendChild(divImg);
-
-    //card - digimon level
-    let divCardContent = document.createElement('div');
-    let divInfo = document.createElement('div');
-    let h2 = document.createElement('h2');
-    let span = document.createElement('span');
-
-    divCardContent.classList.add('content');
-    divInfo.classList.add('info');
-    h2.innerText = digimon.name;
-    span.innerText = digimon.level;
-    divInfo.appendChild(h2);
-    divInfo.appendChild(span);
-    divCardContent.appendChild(divInfo);
-
-    divCard.appendChild(divCardContent);
-  });
 }
 
-const navBarAnchors = document.querySelectorAll('.nav-items a');
+getAllDigimons();
 
-let fetchLevelDigimon = async (level, id) => {
-  closeMenu();
-  let searchInput = document.getElementById('searchInput').value = '';
-  document.getElementById('searchInput').innerText = '';
-  let url = `https://digimon-api.vercel.app/api/digimon/level/${level}`;
-  navBarAnchors.forEach((element) => {
-    if (element.innerHTML === level) {
-      navBarAnchors.forEach((element) => element.classList.remove('active'));
-      element.classList.add('active');
-    }
-  });
-  displayNoneDigimon();
-  id.classList.remove('hide');
-
-  let response = await fetch(url);
-  let data = await response.json();
-
-  data.map((digimon) => {
-    //card element
-    let divCard = document.createElement('div');
-    divCard.className = 'container';
-    id.appendChild(divCard);
-
-    //div image
-    let divImg = document.createElement('div');
-    let img = document.createElement('img');
-    divImg.classList.add('image');
-    img.setAttribute('src', `${digimon.img}`);
-    divImg.appendChild(img);
-    divCard.appendChild(divImg);
-
-    //card - digimon level
-    let divCardContent = document.createElement('div');
-    let divInfo = document.createElement('div');
-    let h2 = document.createElement('h2');
-    let span = document.createElement('span');
-
-    divCardContent.classList.add('content');
-    divInfo.classList.add('info');
-    h2.innerText = digimon.name;
-    span.innerText = digimon.level;
-    divInfo.appendChild(h2);
-    divInfo.appendChild(span);
-    divCardContent.appendChild(divInfo);
-
-    divCard.appendChild(divCardContent);
-    titleMain.innerText = `Digimons level: ${digimon.level}`;
-  });
-};
-
-let fetchNameDigimon = async (name, id) => {
-  id.classList.remove('hide');
-  let url = `https://digimon-api.vercel.app/api/digimon/name/${name}`;
-
-  let response = await fetch(url);
-  let data = await response.json();
-
-  data.map((digimon) => {
-    //card element
-    let divCard = document.createElement('div');
-    divCard.className = 'container';
-    id.appendChild(divCard);
-
-    //div image
-    let divImg = document.createElement('div');
-    let img = document.createElement('img');
-    divImg.classList.add('image');
-    img.setAttribute('src', `${digimon.img}`);
-    divImg.appendChild(img);
-    divCard.appendChild(divImg);
-
-    //card - digimon level
-    let divCardContent = document.createElement('div');
-    let divInfo = document.createElement('div');
-    let h2 = document.createElement('h2');
-    let span = document.createElement('span');
-
-    divCardContent.classList.add('content');
-    divInfo.classList.add('info');
-    h2.innerText = digimon.name;
-    span.innerText = digimon.level;
-    divInfo.appendChild(h2);
-    divInfo.appendChild(span);
-    divCardContent.appendChild(divInfo);
-
-    divCard.appendChild(divCardContent);
-    titleMain.innerText = `Resultado: `;
-  });
-};
-
-async function searchDigimon() {
-  displayNoneDigimon();
-  search.classList.add('hide');
+function showAllDigimon() {
+  titleMain.innerText = 'Lista com todos os Digimons';
 
   let arrayDigimon = JSON.parse(localStorage.getItem('digimon'));
-
-  let searchInput = document.getElementById('searchInput').value;
-  clearSearchContainer();
 
   let filterResult = arrayDigimon.filter(
     (digimon) =>
       digimon.name.toLowerCase().match(searchInput) ||
       digimon.name.match(searchInput),
   );
+
+  filterResult.forEach((digimon) => {
+    let name = digimon.name;
+    let level = digimon.level;
+    let img = digimon.img;
+
+    createContainerSearch(all, img, name, level);
+  });
+}
+
+showAllDigimon();
+
+function clickShowAll() {
+  displayNoneDigimon();
+  all.classList.remove('hide');
+  titleMain.innerText = 'Lista com todos os Digimons';
+  document.getElementById('searchInput').value = '';
+  activeNav();
+}
+
+function clickShowTraining() {
+  displayNoneDigimon();
+  training.classList.remove('hide');
+  activeNav('In Training');
+  titleMain.innerText = 'Filtro: In Training';
+}
+
+function clickShowRookie() {
+  displayNoneDigimon();
+  rookie.classList.remove('hide');
+  activeNav('Rookie');
+  titleMain.innerText = 'Filtro: Rookie';
+}
+
+function clickShowChampion() {
+  displayNoneDigimon();
+  champion.classList.remove('hide');
+  activeNav('Champion');
+  titleMain.innerText = 'Filtro: Champion';
+}
+
+function clickShowUltimate() {
+  displayNoneDigimon();
+  ultimate.classList.remove('hide');
+  activeNav('Ultimate');
+  titleMain.innerText = 'Filtro: Ultimate';
+}
+
+function clickShowMega() {
+  displayNoneDigimon();
+  mega.classList.remove('hide');
+  activeNav('Mega');
+  titleMain.innerText = 'Filtro: Mega';
+}
+
+function activeNav(filter) {
+  const navBarAnchors = document.querySelectorAll('.nav-items a');
+  navBarAnchors.forEach((element) => {
+    if (element.innerHTML === filter) {
+      element.classList.add('active');
+    } else {
+      element.classList.remove('active');
+    }
+  });
+}
+
+function showLevelDigimon(level, container) {
+  document.getElementById('searchInput').value = '';
+
+  let searchInput = (document.getElementById('searchInput').value = '');
+
+  let nameFilter = container;
+
+  let arrayDigimon = JSON.parse(localStorage.getItem('digimon'));
+  let filterResult = arrayDigimon.filter(
+    (digimon) =>
+      digimon.name.toLowerCase().match(searchInput) ||
+      digimon.name.match(searchInput),
+  );
+
+  filterResult.forEach((digimon) => {
+    let name = digimon.name;
+    let levelFilter = digimon.level;
+    let img = digimon.img;
+
+    if (level == levelFilter) {
+      createContainerSearch(nameFilter, img, name, levelFilter);
+    }
+  });
+}
+
+showLevelDigimon('In Training', training);
+showLevelDigimon('Training', training);
+showLevelDigimon('Rookie', rookie);
+showLevelDigimon('Champion', champion);
+showLevelDigimon('Ultimate', ultimate);
+showLevelDigimon('Mega', mega);
+
+function searchDigimon() {
+  displayNoneDigimon();
+  clearSearchContainer();
+  search.classList.remove('hide');
+
+  let arrayDigimon = JSON.parse(localStorage.getItem('digimon'));
+
+  let searchInput = document.getElementById('searchInput').value;
+
+  let filterResult = arrayDigimon.filter(
+    (digimon) =>
+      digimon.name.toLowerCase().match(searchInput) ||
+      digimon.name.match(searchInput),
+  );
+
   let nameFilter = '';
+  const navBarAnchors = document.querySelectorAll('.nav-items a');
   navBarAnchors.forEach((element) => {
     if (element.className == 'active') {
       nameFilter = element.innerHTML;
@@ -199,31 +184,16 @@ async function searchDigimon() {
       let name = digimon.name;
       let level = digimon.level;
       let img = digimon.img;
-      if (nameFilter == level || nameFilter == '') {
+
+      if (nameFilter == level) {
+        createContainerSearch(search, img, name, level);
+      }else if (nameFilter == ''){
         createContainerSearch(search, img, name, level);
       }
     });
-  } else {
-    switch (nameFilter) {
-      case 'In Training':
-        fetchLevelDigimon('In Training', training);
-        break;
-      case 'Rookie':
-        fetchLevelDigimon('Rookie', rookie);
-        break;
-      case 'Champion':
-        fetchLevelDigimon('Champion', champion);
-        break;
-      case 'Ultimate':
-        fetchLevelDigimon('Ultimate', ultimate);
-        break;
-      case 'Mega':
-        fetchLevelDigimon('Mega', mega);
-        break;
-        default: 
-      getAllDigimons();
-    }
-  }
+      } else {
+        switchDigimon(nameFilter);
+      }
 }
 
 function displayNoneDigimon() {
@@ -233,10 +203,11 @@ function displayNoneDigimon() {
 }
 
 function clearSearchContainer() {
-  let containerSearch = document.getElementById('getSearchDigimon');
-
-  while (containerSearch.firstChild) {
-    containerSearch.removeChild(containerSearch.firstChild);
+  if (search) {
+    let containerSearch = document.getElementById('getSearchDigimon');
+    while (containerSearch.firstChild) {
+      containerSearch.removeChild(containerSearch.firstChild);
+    }
   }
 }
 
@@ -267,10 +238,30 @@ function createContainerSearch(container, digimonImage, digimon, level) {
   divCardContent.appendChild(divInfo);
 
   divCard.appendChild(divCardContent);
-  titleMain.innerText = `Resultado: `;
 }
 
 function closeMenu() {
   items.classList.remove('active');
 }
-getAllDigimons();
+
+function switchDigimon(filter) {
+  switch (filter) {
+    case 'In Training':
+      clickShowTraining();
+      break;
+    case 'Rookie':
+      clickShowRookie();
+      break;
+    case 'Champion':
+      clickShowChampion();
+      break;
+    case 'Ultimate':
+      clickShowUltimate();
+      break;
+    case 'Mega':
+      clickShowMega();
+      break;
+    default:
+      clickShowAll();
+  }
+}
